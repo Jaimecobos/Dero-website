9f7e1453539c1d3511644abd6bbdd6dda0338e5d
Jaimecobos <jaimecobos1994@gmail.com>
2021-01-23 05:22:29 +0100

    sunglasses emoji changed to bread

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# Importing required libraries, obviously
+
 import streamlit as st
 import cv2
 from PIL import Image
@@ -6,50 +6,8 @@ import numpy as np
 import os
 
 def detect(image):
-    '''
-    Function to detect faces/eyes and smiles in the image passed to this function
-    '''
 
-    
-    image = np.array(image.convert('RGB'))
-    
-    # Next two lines are for converting the image from 3 channel image (RGB) into 1 channel image
-    # img = cv2.cvtColor(new_img, 1)
-    # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    
-    # Passing grayscale image to perform detection
-    # We pass grayscaled image because opencv expects image with one channel
-    # Even if you don't convert the image into one channel, open-cv does it automatically.
-    # So, you can just comment line number 26 and 27.
-    # If you do, make sure that you change the variables name at appropriate places in the code below
-    # Don't blame me if you run into errors while doing that :P
-    
-    #faces = face_cascade.detectMultiScale(image=image, scaleFactor=1.3, minNeighbors=5)
-    # The face_cascade classifier returns coordinates of the area in which the face might be located in the image
-    # These coordinates are (x,y,w,h)
-    # We will be looking for eyes and smile within this area instead of looking for them in the entire image
-    # This makes sense when you're looking for smiles and eyes in a face, if that is not your use case then
-    # you can pull the code segment out and make a different function for doing just that, specifically.
-
-
-    # Draw rectangle around faces
-    #for (x, y, w, h) in faces:
-        
-        # The following are the parameters of cv2.rectangle()
-        # cv2.rectangle(image_to_draw_on, start_point, end_point, color, line_width)
-        #cv2.rectangle(img=image, pt1=(x, y), pt2=(x + w, y + h), color=(255, 0, 0), thickness=2)
-        
-       ##eyes = eye_cascade.detectMultiScale(roi)
-        
-        # Detecting smiles in the face(s) detected
-        ##for (ex,ey,ew,eh) in eyes:
-            #cv2.rectangle(roi, (ex, ey), (ex+ew, ey+eh), (0,255,0), 2)
-            
-        # Drawing rectangle around smile
-        #for (sx,sy,sw,sh) in smile:
-            #cv2.rectangle(roi, (sx, sy), (sx+sw, sy+sh), (0,0,255), 2)
-
-    # Returning the image with bounding boxes drawn on it (in case of detected objects), and faces array
+    # Returning the image with bounding boxes drawn on it (in case of detected objects), and bounding box array
     return image
 
 def about():
