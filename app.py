5826b37bfed25685d3c9da9c9c76dcd9c4dccf55
Jaimecobos <jaimecobos1994@gmail.com>
2021-01-24 21:13:44 +0100

    removed spaces and added a new code for the file_uploader

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 
-import streamlit as st
-import cv2
-from PIL import Image
-import numpy as np
-import os
+try:
+	import streamlit as st
+	import cv2
+	import pandas as pd
+	import numpy as np
+	import os
+	import sys
+	from PIL import Image
+	from io import BytesIO,StringIO
+	print("All Modules Loaded  ")
+except Exception as e:
+	print("Some modules are missing : {} ".format(e))
+
+try:
 
 def detect(image):
 
@@ -13,41 +22,33 @@ def detect(image):
 def about():
 	st.write(
 		'''
-		yo
+		An assignment that is possible due to the Minor Smart Robot Manufacturing. Due to the corona lockdown the assignment was focused on the machine learning aspect of detecting a bread.
 		''')
-
 
 def main():
     st.title("Bread Detection App :Bread: ")
     st.write("**Bounding box detection using keras and tenserflow**")
 
-    activities = ["Home", "About"]
-    choice = st.sidebar.selectbox("Pick something fun", activities)
+    activities = ["Home", "Dataset", "About"]
+    choice = st.sidebar.selectbox("Pick something", activities)
 
     if choice == "Home":
-
     	st.write("Go to the About section from the sidebar to learn more about it.")
         
         # You can specify more file types below if you want
     	image_file = st.file_uploader("Upload image", type=['jpeg', 'png', 'jpg', 'webp'])
-
     	if image_file is not None:
-
     		image = Image.open(image_file)
 
     		if st.button("Process"):
-                
                 # result_img is the image with rectangle drawn on it (in case there are bread detected)
                 # result_faces is the array with co-ordinates of bounding box(es)
-    			result_img, result_bread = detect(image=image)
+    			#result_img, result_bread = detect(image=image)
     			st.image(result_img, use_column_width = True)
     			st.success("Found {} bread\n".format(len(result_bread)))
 
     elif choice == "About":
     	about()
-
-
-
 
 if __name__ == "__main__":
     main()
