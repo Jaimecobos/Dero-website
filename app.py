ca57faa646f8789258c420f668e7df6bfcb49ed8
Jaimecobos <jaimecobos1994@gmail.com>
2021-01-24 21:23:04 +0100

    removed spaces and added a new code for the file_uploader

--- app.py
+++ app.py
@@ -14,8 +14,8 @@ except Exception as e:
 
 def detect(image):
 
-    # Returning the image with bounding boxes drawn on it (in case of detected objects), and bounding box array
-    return image
+	# Returning the image with bounding boxes drawn on it (in case of detected objects), and bounding box array
+	return image
 
 def about():
 	st.write(
@@ -24,29 +24,27 @@ def about():
 		''')
 
 def main():
-    st.title("Bread Detection App :Bread: ")
-    st.write("**Bounding box detection using keras and tenserflow**")
+	st.title("Bread Detection App :Bread: ")
+	st.write("**Bounding box detection using keras and tenserflow**")
 
-    activities = ["Home", "Dataset", "About"]
-    choice = st.sidebar.selectbox("Pick something", activities)
+	activities = ["Home", "Dataset", "About"]
+	choice = st.sidebar.selectbox("Pick something", activities)
 
-    if choice == "Home":
-    	st.write("Go to the About section from the sidebar to learn more about it.")
-        
-        # You can specify more file types below if you want
-    	image_file = st.file_uploader("Upload image", type=['jpeg', 'png', 'jpg', 'webp'])
-    	if image_file is not None:
-    		image = Image.open(image_file)
+	if choice == "Home":
+		st.write("Go to the About section from the sidebar to learn more about it.")
 
-    		if st.button("Process"):
-                # result_img is the image with rectangle drawn on it (in case there are bread detected)
-                # result_faces is the array with co-ordinates of bounding box(es)
-    			#result_img, result_bread = detect(image=image)
-    			st.image(result_img, use_column_width = True)
-    			st.success("Found {} bread\n".format(len(result_bread)))
+		# Specify the files types.
+		image_file = st.file_uploader("Upload image", type=['jpeg', 'png', 'jpg', 'webp'])
+		if image_file is not None:
+			image = Image.open(image_file)
 
-    elif choice == "About":
-    	about()
+			if st.button("Process"):
+				result_img = load_image (image_file)
+				st.image(result_img, use_column_width = True)
+				st.success("Found {} bread\n".format(len(result_bread)))
+
+	elif choice == "About":
+		about()
 
 if __name__ == "__main__":
-    main()
+	main()
