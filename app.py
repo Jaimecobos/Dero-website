cf8b1c8d2fd1c4cede60295583d4adc3d215a08f
Elmer <16076567@student.hhs.nl>
2021-01-25 13:29:04 +0100

    adadfadsadfs

--- app.py
+++ app.py
@@ -6,16 +6,51 @@ import os
 import sys
 import requests
 from PIL import Image
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-from visualization import visualize
+import matplotlib.pyplot as plt
 
 
-def detect(image):
+def visualize(image, pred_labels):
+    '''
+    Visualization tool to plot images with ground truth boxes and/or predicted labels
 
-	# Returning the image with bounding boxes drawn on it (in case of detected objects), and bounding box array
-	return image
+    Arguments:
+        image (numpy array) : the image to plot
+        gt_labels (np array, optional) : the ground truth labels, this will be plot in red
+        pred_labels (np array, optional) : the predicted labels, this will be plot in blue
+        save_folder (string, optional) : if set, the image is not plot but saved with this path
+
+    '''
+
+    plt.figure(figsize=(12, 12))
+    plt.imshow(image)
+    current_axis = plt.gca()
+
+    current_axis.text(1, -10, '- Predicted boxes bread roll top', size='x-large', color='green', bbox={'facecolor': 'white', 'alpha': 1.0})
+    current_axis.text(100, -10, '- Predicted boxes bread roll bottom', size='x-large', color='red', bbox={'facecolor': 'white', 'alpha': 1.0})
+
+    for box in pred_labels:
+        class_id = box[0]
+        cx = box[2]
+        cy = box[3]
+        w = box[4]
+        h = box[5]
+        angle = box[6]
+
+        xmin, ymin = cx - 1 / 2 * (-h * np.sin(angle) + w * np.cos(angle)), \
+                     cy - 1 / 2 * (h * np.cos(angle) + w * np.sin(angle))
+
+        if class_id == 1:
+            color = 'green'
+        else:
+            color = 'red'
+
+        current_axis.add_patch(
+            plt.Rectangle((xmin, ymin), w, h, angle=angle * 180 / np.pi, color=color, fill=False, linewidth=2))
+
+        label = '{:.2f}'.format(box[1])
+        current_axis.text(xmin, ymin, label, size='x-small', color='black', bbox={'facecolor': color, 'alpha': 1.0})
+
+    return plt.gcf()
 
 
 def about():
