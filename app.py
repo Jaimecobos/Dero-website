7e2f096ab3507d0ffdce3bbceddbc743b03e56f2
Elmer <16076567@student.hhs.nl>
2021-01-25 13:25:30 +0100

    Add main directory to python path

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ import os
 import sys
 import requests
 from PIL import Image
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from visualization import visualize
 
 
